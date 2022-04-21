9eccc44e67fa47767c2bbfdb4b01d869d498f5e6
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-04-21 13:48:40 +0800

    Completed app

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS employee_db;
+DROP DATABASE IF EXISTS employee_DB;
 
-CREATE DATABASE employee_db;
+CREATE DATABASE employee_DB;
 
-USE employee_db;
+USE employee_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
@@ -42,8 +42,8 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Rep", 90500.00, 1), 
 ("Sales Supervisor", 120000.00, 1), 
 ("Software Engineer", 115000.00, 3), 
-("Lead Accountant", 130000.00, 2),
-("Intern", 70000.00, 4);
+("Lead Accountant", 140000.00, 2),
+("Custodian", 70000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Wick", 2, 1), ("Holly", "Sanders", 1, 1), ("Will", "Smith", 3, 2), ("Tony", "Montana", 5, 3), ("Jessica", "Alba", 4, 4);
+VALUES ("John", "Snow", 2, 1), ("Holly", "Smith", 1, 1), ("William", "Broboski", 3, 2), ("Alex", "Hall", 5, 3), ("Margie", "Holland", 4, 4);
